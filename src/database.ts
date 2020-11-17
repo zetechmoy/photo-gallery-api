66c4a2bdbc9a2441524063dd3de8ae46458bde50
azaydeb <theoguidoux@gmail.com>
2020-11-17 22:35:00 +0100

    docker build

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import { connect } from 'mongoose'
 
 export async function startConnection() {
-    const db = await connect('mongodb://localhost/mean-gallery',{
+    const db = await connect('mongodb://mongo:27017/mean-gallery',{
         useNewUrlParser: true,
         useFindAndModify: false 
     });
